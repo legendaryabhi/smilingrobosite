bc796c7494dc0dbcdf3ef5568ce8b8d1b71b0ffd
tanishk <tanishk2250@gmail.com>
2024-06-12 11:41:04 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    images: { unoptimized: true } ,
+    compiler:{
+        removeConsole: true,
+    }
+};
 
 export default nextConfig;
